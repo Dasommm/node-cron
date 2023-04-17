610a0f5324fb1bbdd774a78d2628b7cc44b074bb
Jerry <jerry.kwon@peopet.co.kr>
2023-04-17 19:05:11 +0900

    [FEAT]add log library

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const { timeLog, log } = require('console');
+const winston = require('winston');
 var cron = require('node-cron');    //import 같은 개념
+
+const logFilePath = 'logs/app.log';
+
+const logger = winston.createLogger({
+    level : 'info',
+    format : winston.format.json(),
+    transports : [
+        new winston.transports.File({ filename : logFilePath })
+    ]
+})
+let today = new Date();
 
 cron.schedule('* * * * *', () => {
   console.log('running a task every minute');
+  logger.info(today.toLocaleTimeString, "running!!!");
 });
